730023a6d2753a5e757e5d22220674c62bff7f98
Justin Large <justinlarge1974@gmail.com>
2017-06-14 20:38:45 -0700

    Got the recall stuff working again

--- askJakeLambda.js
+++ askJakeLambda.js
@@ -14,7 +14,7 @@ const Alexa = require('alexa-sdk');
 const AWS = require('aws-sdk');
 const https = require("https");
 
-const apiKey = "c666snh3xxe9yp2fgf7zxysj";
+const apiKey = "68da4xqgp74rvhph299vsebk";
 
 const handlers = {
   'YearMakeModel': function() {
@@ -24,11 +24,11 @@ const handlers = {
     let make = slots.make.value;
     let model = slots.model.value;
 
-    getCar(year, make, model, this);
+    storeCar(year, make, model, this);
     this.emit(':tell', 'congrats, noted' );
   },
-'GetRecalls': function() {
-   getRecalls(this);
+  'GetRecalls': function() {
+    getRecalls(this);
 
   },
   'AMAZON.HelpIntent': function() {
@@ -45,30 +45,41 @@ const handlers = {
 };
 
 function getRecalls(alexaContext) {
-   console.log('Were in getRecalls');
-   var docClient = new AWS.DynamoDB.DocumentClient();
+  console.log('Were in getRecalls');
+  var docClient = new AWS.DynamoDB.DocumentClient();
 
   var table = "AskJakeCarMaintenance";
 
-  let options = {
-    hostname: "api.edmunds.com",
-    path: `v1/api/maintenance/recallrepository/findbymodelyearid?modelyearid=100523475&fmt=json&api_key=${apiKey}`
-
+  var params = {
+    TableName: table
   };
 
-  console.log("starting request to " + options.path);
+  console.log("Scanning table.");
+  docClient.scan(params, onScan);
 
-  https.request('https://api.edmunds.com/v1/api/maintenance/recallrepository/findbymodelyearid?modelyearid=100523475&fmt=json&api_key=68da4xqgp74rvhph299vsebk', (res) => {
-    console.log('finished get.  status = ' + res.status);
-    res.on('data', (d) => {
-      var data = JSON.parse(d);
-      console.log(''+ data.recallHolder.length);
-      alexaContext.emit(':tell', `you have ${data.recallHolder.length} recalls`);
-    });
-  }).end();
+  function onScan(err, data) {
+    if (err) {
+      console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+      // print id
+      // console.log("Scan succeeded." + JSON.stringify(data.Items[0].modelYearId));
+      // let url = `https://api.edmunds.com/v1/api/maintenance/recallrepository/findbymodelyearid?modelyearid=${data.Items[0].modelYearId}&fmt=json&api_key=68da4xqgp74rvhph299vsebk`;
+
+      let url = `https://api.edmunds.com/v1/api/maintenance/recallrepository/findbymodelyearid?modelyearid=100523475&fmt=json&api_key=68da4xqgp74rvhph299vsebk`;
+      https.request(url, (res) => {
+        console.log('finished get.  status = ' + res.status);
+        res.on('data', (d) => {
+          var data = JSON.parse(d);
+          console.log(''+ data.recallHolder.length);
+          // TODO:  Send a text
+          alexaContext.emit(':tell', `you have ${data.recallHolder.length} recalls.  Sending a text with more information`);
+        });
+      }).end();
+    }
+  }
 }
 
-function getCar(year, make, model) {
+function storeCar(year, make, model) {
   var docClient = new AWS.DynamoDB.DocumentClient();
 
   var table = "AskJakeCarMaintenance";
@@ -77,12 +88,17 @@ function getCar(year, make, model) {
     hostname: "api.edmunds.com",
     path: `/api/vehicle/v2/${make}/${model}/${year}?view=full&fmt=json&api_key=${apiKey}`
   };
+
   console.log('make: '+make+ ' model: '+model+' year: '+year);
 
   console.log("starting request to " + options.path);
 
   https.request(options, (res) => {
     console.log('finished get.  status = ' + res.statusCode);
+    res.on('error', (e) => {
+      console.log(JSON.stringify(e));
+    });
+
     res.on('data', (d) => {
       console.log('data from edmunds:  ' + d);
       var data = JSON.parse(d);
