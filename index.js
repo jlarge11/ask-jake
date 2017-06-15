6496b4bd9814dec254ced2cf4f93a024ef2ed6db
Matthew Abbruscato <mabbrus@gmail.com>
2017-06-15 01:28:41 -0700

    Finished Product

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 /* eslint-disable  func-names */
 /* eslint quote-props: ["error", "consistent"]*/
 /**
-* This sample demonstrates a simple skill built with the Amazon Alexa Skills
-* nodejs skill development kit.
-* This sample supports multiple lauguages. (en-US, en-GB, de-DE).
-* The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
-* as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
-**/
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills
+ * nodejs skill development kit.
+ * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
+ * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
+ * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
+ **/
 
 'use strict';
 
 const Alexa = require('alexa-sdk');
+const Axios = require('axios');
 const AWS = require('aws-sdk');
 const https = require("https");
 
@@ -18,7 +19,7 @@ const apiKey = "f7cg9hpv9wkj65nkh5m7e7fm";
 
 const handlers = {
   'Khakis': function() {
-    this.emit(':tell', 'Uhh... Khakis?' );
+    this.emit(':tell', `<prosody rate="50%" pitch="low">auh...</prosody><prosody rate="slow" pitch="low"> Khakis?.</prosody>`);
   },
   'YearMakeModel': function() {
     console.log('howdy ho!');
@@ -27,11 +28,38 @@ const handlers = {
     let make = slots.make.value;
     let model = slots.model.value;
 
-    storeCar(year, make, model, this);
-    this.emit(':tell', 'congrats, noted' );
+    let table = "AskJakeDB";
+    getCar(year, make, model).then((response) => {
+      console.log(response.data.id);
+
+      var params = {
+        TableName: table,
+        Item: {
+          "modelYearId": response.data.id
+        }
+      };
+
+      console.log(params);
+      console.log("Adding a new item...");
+      createNewVehicle(params).then(() => {
+        this.emit(':tell', `Congrats, Noted`);
+        console.log('Finished');
+      });
+    });
   },
   'GetRecalls': function() {
-    getRecalls(this);
+    getRecalls().then((resolved) => {
+      let cardTitle = "Your Vehicle's Recalls";
+      let speechOutput = `There are ${resolved.data.recallHolder.length} recalls on your vehicle.  I'll send you more information`;
+      let cardContent = '';
+
+      resolved.data.recallHolder.forEach((recallItem) => {
+        cardContent += recallItem.recallNumber + '\r\n';
+      });
+
+      cardContent += '\r\nYou can contact your nearest Ford Service Center at 309-555-9876';
+      this.emit(':tellWithCard', speechOutput, cardTitle, cardContent);
+    });
   },
   'GetAgent': function() {
     let agentName = 'Mary Contreras';
@@ -64,92 +92,50 @@ const handlers = {
   }
 };
 
-function getRecalls(alexaContext) {
+function createNewVehicle(params) {
+  let docClient = new AWS.DynamoDB.DocumentClient();
+  return docClient.put(params).promise();
+}
+
+function getRecalls() {
   console.log('Were in getRecalls');
-  var docClient = new AWS.DynamoDB.DocumentClient();
+  let docClient = new AWS.DynamoDB.DocumentClient();
 
-  var table = "AskJakeCarMaintenance";
+  let table = "AskJakeDB";
 
-  var params = {
+  let params = {
     TableName: table
   };
 
   console.log("Scanning table.");
-  docClient.scan(params, onScan);
 
-  function onScan(err, data) {
+  return docClient.scan(params).promise().then((data, err) => {
     if (err) {
       console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+      return new Promise().reject('Scan failed.');
     } else {
       // print id
-      // console.log("Scan succeeded." + JSON.stringify(data.Items[0].modelYearId));
-      // let url = `https://api.edmunds.com/v1/api/maintenance/recallrepository/findbymodelyearid?modelyearid=${data.Items[0].modelYearId}&fmt=json&api_key=68da4xqgp74rvhph299vsebk`;
-
-      let url = `https://api.edmunds.com/v1/api/maintenance/recallrepository/findbymodelyearid?modelyearid=100523475&fmt=json&api_key=${apiKey}`;
-      https.request(url, (res) => {
-        console.log('finished get.  status = ' + res.status);
-        res.on('data', (d) => {
-          var data = JSON.parse(d);
-          console.log(''+ data.recallHolder.length);
-          let cardTitle = "Your Vehicle's Recalls";
-          let speechOutput = `There are ${data.recallHolder.length} recalls on your vehicle.  I'll send you more information`;
-          let cardContent = '';
-
-          data.recallHolder.forEach((recallItem) => {
-            cardContent += recallItem.recallNumber + '\r\n';
-          });
-
-          cardContent += '\r\nYou can contact your nearest Ford Service Center at 309-555-9876';
-
-          alexaContext.emit(':tellWithCard', speechOutput, cardTitle, cardContent);
-        });
-      }).end();
+      console.log("Scan succeeded." + JSON.stringify(data.Items[0].modelYearId));
+      return Axios({
+        method: 'get',
+        url: `https://api.edmunds.com/v1/api/maintenance/recallrepository/findbymodelyearid?modelyearid=${data.Items[0].modelYearId}&fmt=json&api_key=${apiKey}`
+      });
     }
-  }
+  });
 }
 
-function storeCar(year, make, model) {
-  var docClient = new AWS.DynamoDB.DocumentClient();
+function getCar(year, make, model) {
 
-  var table = "AskJakeCarMaintenance";
-
+  console.log("starting request");
   let options = {
-    hostname: "api.edmunds.com",
+    hostname: "https://api.edmunds.com",
     path: `/api/vehicle/v2/${make}/${model}/${year}?view=full&fmt=json&api_key=${apiKey}`
   };
-
-  console.log('make: '+make+ ' model: '+model+' year: '+year);
-
-  console.log("starting request to " + options.path);
-
-  https.request(options, (res) => {
-    console.log('finished get.  status = ' + res.statusCode);
-    res.on('error', (e) => {
-      console.log(JSON.stringify(e));
-    });
-
-    res.on('data', (d) => {
-      console.log('data from edmunds:  ' + d);
-      var data = JSON.parse(d);
-      //used to create the table
-      var params = {
-        TableName: table,
-        Item: {
-          "modelYearId": data.id
-        }
-      };
-
-      console.log("Putting the following to the database:  " + JSON.stringify(params));
-
-      docClient.put(params, function(err, data) {
-        if (err) {
-          console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-        } else {
-          console.log("Added item:", JSON.stringify(data, null, 2));
-        }
-      });
-    });
-  }).end();
+  console.log(options.path);
+  return Axios({
+    method: 'get',
+    url: options.hostname + options.path
+  });
 }
 
 exports.handler = function(event, context) {
