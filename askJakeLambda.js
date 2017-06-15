af1de4d5a64e2ff1a772c05c60e4e8c721e73ac1
Justin Large <justinlarge1974@gmail.com>
2017-06-14 17:34:16 -0700

    Finally got the hookup between Edmunds and the write to our own database

--- askJakeLambda.js
+++ askJakeLambda.js
@@ -11,149 +11,78 @@
 'use strict';
 
 const Alexa = require('alexa-sdk');
+const AWS = require('aws-sdk');
+const https = require("https");
+const http = require("http");
 
-const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
-
-const languageStrings = {
-  'en': {
-    translation: {
-      FACTS: [
-        'A year on Mercury is just 88 days long.',
-        'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-        'Venus rotates anti-clockwise, possibly because of a collision in the past with an asteroid.',
-        'On Mars, the Sun appears about half the size as it does on Earth.',
-        'Earth is the only planet not named after a god.',
-        'Jupiter has the shortest day of all the planets.',
-        'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-        'The Sun contains 99.86% of the mass in the Solar System.',
-        'The Sun is an almost perfect sphere.',
-        'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-        'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-        'The temperature inside the Sun can reach 15 million degrees Celsius.',
-        'The Moon is moving approximately 3.8 cm away from our planet every year.',
-      ],
-      SKILL_NAME: 'Space Facts',
-      GET_FACT_MESSAGE: "Here's your fact: ",
-      HELP_MESSAGE: 'You can say tell me a space fact, or, you can say exit... What can I help you with?',
-      HELP_REPROMPT: 'What can I help you with?',
-      STOP_MESSAGE: 'Goodbye!',
-    },
-  },
-  'en-US': {
-    translation: {
-      FACTS: [
-        'A year on Mercury is just 88 days long.',
-        'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-        'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
-        'On Mars, the Sun appears about half the size as it does on Earth.',
-        'Earth is the only planet not named after a god.',
-        'Jupiter has the shortest day of all the planets.',
-        'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-        'The Sun contains 99.86% of the mass in the Solar System.',
-        'The Sun is an almost perfect sphere.',
-        'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-        'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-        'The temperature inside the Sun can reach 15 million degrees Celsius.',
-        'The Moon is moving approximately 3.8 cm away from our planet every year.',
-      ],
-      SKILL_NAME: 'American Space Facts',
-    },
-  },
-  'en-GB': {
-    translation: {
-      FACTS: [
-        'A year on Mercury is just 88 days long.',
-        'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-        'Venus rotates anti-clockwise, possibly because of a collision in the past with an asteroid.',
-        'On Mars, the Sun appears about half the size as it does on Earth.',
-        'Earth is the only planet not named after a god.',
-        'Jupiter has the shortest day of all the planets.',
-        'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-        'The Sun contains 99.86% of the mass in the Solar System.',
-        'The Sun is an almost perfect sphere.',
-        'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-        'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-        'The temperature inside the Sun can reach 15 million degrees Celsius.',
-        'The Moon is moving approximately 3.8 cm away from our planet every year.',
-      ],
-      SKILL_NAME: 'British Space Facts',
-    },
-  },
-  'de': {
-    translation: {
-      FACTS: [
-        'Ein Jahr dauert auf dem Merkur nur 88 Tage.',
-        'Die Venus ist zwar weiter von der Sonne entfernt, hat aber höhere Temperaturen als Merkur.',
-        'Venus dreht sich entgegen dem Uhrzeigersinn, möglicherweise aufgrund eines früheren Zusammenstoßes mit einem Asteroiden.',
-        'Auf dem Mars erscheint die Sonne nur halb so groß wie auf der Erde.',
-        'Die Erde ist der einzige Planet, der nicht nach einem Gott benannt ist.',
-        'Jupiter hat den kürzesten Tag aller Planeten.',
-        'Die Milchstraßengalaxis wird in etwa 5 Milliarden Jahren mit der Andromeda-Galaxis zusammenstoßen.',
-        'Die Sonne macht rund 99,86 % der Masse im Sonnensystem aus.',
-        'Die Sonne ist eine fast perfekte Kugel.',
-        'Eine Sonnenfinsternis kann alle ein bis zwei Jahre eintreten. Sie ist daher ein seltenes Ereignis.',
-        'Der Saturn strahlt zweieinhalb mal mehr Energie in den Weltraum aus als er von der Sonne erhält.',
-        'Die Temperatur in der Sonne kann 15 Millionen Grad Celsius erreichen.',
-        'Der Mond entfernt sich von unserem Planeten etwa 3,8 cm pro Jahr.',
-      ],
-      SKILL_NAME: 'Weltraumwissen auf Deutsch',
-      GET_FACT_MESSAGE: 'Hier sind deine Fakten: ',
-      HELP_MESSAGE: 'Du kannst sagen, „Nenne mir einen Fakt über den Weltraum“, oder du kannst „Beenden“ sagen... Wie kann ich dir helfen?',
-      HELP_REPROMPT: 'Wie kann ich dir helfen?',
-      STOP_MESSAGE: 'Auf Wiedersehen!',
-    },
-  },
-};
+const apiKey = "c666snh3xxe9yp2fgf7zxysj";
 
 const handlers = {
-  'YearMakeModel': function () {
+  'YearMakeModel': function() {
+    console.log('howdy ho!');
     let slots = this.event.request.intent.slots;
     let year = slots.year.value;
     let make = slots.make.value;
     let model = slots.model.value;
 
-    this.emit(':tell', `year is ${year} make is ${make} model is ${model}`);
+    getCar(year, make, model, this);
   },
 
-  'Ping': function () {
-    let personName = this.event.request.intent.slots.personName.value;
-    this.emit(':tell', 'Pong ' + personName);
-  },
-  'LaunchRequest': function () {
-    this.emit('GetFact');
-  },
-  'GetNewFactIntent': function () {
-    this.emit('GetFact');
-  },
-  'GetFact': function () {
-    // Get a random space fact from the space facts list
-    // Use this.t() to get corresponding language data
-    const factArr = this.t('FACTS');
-    const factIndex = Math.floor(Math.random() * factArr.length);
-    const randomFact = factArr[factIndex];
-
-    // Create speech output
-    const speechOutput = this.t('GET_FACT_MESSAGE') + randomFact;
-    this.emit(':tellWithCard', speechOutput, this.t('SKILL_NAME'), randomFact);
-  },
-  'AMAZON.HelpIntent': function () {
+  'AMAZON.HelpIntent': function() {
     const speechOutput = this.t('HELP_MESSAGE');
     const reprompt = this.t('HELP_MESSAGE');
     this.emit(':ask', speechOutput, reprompt);
   },
-  'AMAZON.CancelIntent': function () {
+  'AMAZON.CancelIntent': function() {
     this.emit(':tell', this.t('STOP_MESSAGE'));
   },
-  'AMAZON.StopIntent': function () {
+  'AMAZON.StopIntent': function() {
     this.emit(':tell', this.t('STOP_MESSAGE'));
-  },
+  }
 };
 
-exports.handler = function (event, context) {
+function getCar(year, make, model) {
+  var docClient = new AWS.DynamoDB.DocumentClient();
+
+  var table = "AskJakeCarMaintenance";
+
+  let options = {
+    hostname: "api.edmunds.com",
+    path: `/api/vehicle/v2/${make}/${model}/${year}?view=full&fmt=json&api_key=${apiKey}`
+  };
+  console.log('make: '+make+ ' model: '+model+' year: '+year);
+
+  console.log("starting request to " + options.path);
+
+  https.request(options, (res) => {
+    console.log('finished get.  status = ' + res.statusCode);
+    res.on('data', (d) => {
+      console.log('data from edmunds:  ' + d);
+      var data = JSON.parse(d);
+      //used to create the table
+      var params = {
+        TableName: table,
+        Item: {
+          "modelYearId": data.id
+        }
+      };
+
+      console.log("Putting the following to the database:  " + JSON.stringify(params));
+
+      docClient.put(params, function(err, data) {
+        if (err) {
+          console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+          console.log("Added item:", JSON.stringify(data, null, 2));
+        }
+      });
+    });
+  }).end();
+}
+
+exports.handler = function(event, context) {
   const alexa = Alexa.handler(event, context);
-  alexa.APP_ID = APP_ID;
-  // To enable string internationalization (i18n) features, set a resources object.
-  alexa.resources = languageStrings;
+  alexa.APP_ID = undefined;
   alexa.registerHandlers(handlers);
   alexa.execute();
 };
